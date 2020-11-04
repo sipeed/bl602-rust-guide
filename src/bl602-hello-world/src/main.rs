c305219441fa472579e0c04dc9bd27ecf0b6a26a
luojia65 <me@luojia.cc>
2020-11-04 21:09:05 +0800

    Green LED on board is on

--- src/bl602-hello-world/src/main.rs
+++ src/bl602-hello-world/src/main.rs
@@ -15,14 +15,14 @@ use panic_halt as _;
 #[riscv_rt::entry]
 fn main() -> ! {
     let dp = pac::Peripherals::take().unwrap();
-    dp.GLB.gpio_cfgctl8.modify(|_, w| unsafe { w
-        .reg_gpio_17_func_sel().bits(11) // GPIO_FUN_SWGPIO
-        .reg_gpio_17_ie().clear_bit() // output
-        .reg_gpio_17_pu().clear_bit()
-        .reg_gpio_17_pd().clear_bit()
-        .reg_gpio_17_drv().bits(0) // disabled
-        .reg_gpio_17_smt().clear_bit()
+    dp.GLB.gpio_cfgctl2.modify(|_, w| unsafe { w
+        .reg_gpio_5_func_sel().bits(11) // GPIO_FUN_SWGPIO
+        .reg_gpio_5_ie().clear_bit() // output
+        .reg_gpio_5_pu().clear_bit()
+        .reg_gpio_5_pd().clear_bit()
+        .reg_gpio_5_drv().bits(0) // disabled
+        .reg_gpio_5_smt().clear_bit()
     });
-    dp.GLB.gpio_cfgctl32.modify(|_, w| w.reg_gpio_17_o().set_bit());
+    dp.GLB.gpio_cfgctl32.modify(|_, w| w.reg_gpio_5_o().set_bit());
     loop {}
 }
