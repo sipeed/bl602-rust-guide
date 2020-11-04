fd9eb4e4df00804fc193939fdd8bd3df88f931bd
luojia65 <me@luojia.cc>
2020-11-04 20:08:57 +0800

    Project structure

--- src/bl602-hello-world/src/main.rs
+++ src/bl602-hello-world/src/main.rs
@@ -3,6 +3,16 @@
 
 use bl602_hal::pac;
 
+use panic_halt as _;
+
+/*
+    bl_gpio_enable_output(DEMO_GPIO_PIN, 0, 0);
+    bl_gpio_output_set(DEMO_GPIO_PIN, 0);
+    
+    log_debug("led on");
+    bl_gpio_output_set(DEMO_GPIO_PIN, 1);
+*/
+
 #[riscv_rt::entry]
 fn main() -> ! {
     loop {}
