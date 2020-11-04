ce4dd81ba0e6c6c5cfc993ac0142d23ffb526fb3
luojia65 <me@luojia.cc>
2020-11-04 20:52:00 +0800

    Small fix

--- src/bl602-hello-world/src/main.rs
+++ src/bl602-hello-world/src/main.rs
@@ -7,13 +7,14 @@ use panic_halt as _;
 
 /*
     bl_gpio_enable_output(DEMO_GPIO_PIN, 0, 0);
-    bl_gpio_output_set(DEMO_GPIO_PIN, 0);
     
     log_debug("led on");
-    bl_gpio_output_set(DEMO_GPIO_PIN, 1);
+    bl_gpio_output_set(DEMO_GPIO_PIN, 1); // 0x40000188
 */
 
 #[riscv_rt::entry]
 fn main() -> ! {
+    let dp = pac::Peripherals::take().unwrap();
+    dp.GLB.gpio_cfgctl32.modify(|_, w| w.reg_gpio_17_o().set_bit());
     loop {}
 }
