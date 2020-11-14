5c14b68e327eff5385e544a4ff383256cb34fd3b
luojia65 <me@luojia.cc>
2020-11-14 20:17:20 +0800

    Try again

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #![no_std]
 #![no_main]
 
-use bl602_hal::pac;
+use bl602_hal::{serial::*, pac, prelude::*};
 
 use panic_halt as _;
 
@@ -25,69 +25,27 @@ fn main() -> ! {
     dp.UART.data_config.write(|w| w
         .cr_uart_bit_inv().clear_bit()
     );
-    // 8N1
-    /* 4->5b 5->6b 6->7b 7->8b */
-    let data_bits_cfg = 7; // 8 bits
-    /* 0->0.5b 1->1b 2->1.5b 3->2b */
-    let stop_bits_cfg = 1; // todo: check this parameter
-    dp.UART.utx_config.write(|w| unsafe { w
-        .cr_utx_prt_en().clear_bit() // parity: none
-        .cr_utx_bit_cnt_d().bits(data_bits_cfg)
-        .cr_utx_bit_cnt_p().bits(stop_bits_cfg) 
-        .cr_utx_frm_en().clear_bit() // [!] freerun off
-        // .cr_utx_len().bits(1) // todo
-        .cr_utx_cts_en().clear_bit() // no CTS
-        .cr_utx_en().set_bit() // enable TX
-    });
-    dp.UART.urx_config.write(|w| unsafe { w
-        .cr_urx_prt_en().clear_bit() // parity: none
-        .cr_urx_bit_cnt_d().bits(data_bits_cfg)
-        .cr_urx_deg_en().clear_bit() // no rx input de-glitch
-        .cr_urx_rts_sw_mode().clear_bit() // no RTS
-        .cr_urx_en().set_bit() // enable RX
-    });
-    // set gpio configuration
-    // tx pin
-    dp.GLB.gpio_cfgctl8.modify(|_, w| unsafe { w
-        .reg_gpio_16_func_sel().bits(7) // GPIO_FUN_UART
-        .reg_gpio_16_ie().set_bit() // input
-        .reg_gpio_16_pu().set_bit() // pull up enable
-        .reg_gpio_16_pd().clear_bit()
-        .reg_gpio_16_drv().bits(0) // disabled
-        .reg_gpio_16_smt().clear_bit()
-    });
-    // rx pin
-    dp.GLB.gpio_cfgctl3.modify(|_, w| unsafe { w
-        .reg_gpio_7_func_sel().bits(7) // GPIO_FUN_UART
-        .reg_gpio_7_ie().set_bit() // input
-        .reg_gpio_7_pu().set_bit() // pull up enable
-        .reg_gpio_7_pd().clear_bit()
-        .reg_gpio_7_drv().bits(0) // disabled
-        .reg_gpio_7_smt().clear_bit()
-    });
-    dp.GLB.uart_sig_sel_0.write(|w| unsafe { w
-        .uart_sig_0_sel().bits(2) // tx -> GLB_UART_SIG_FUN_UART0_TXD
-        .uart_sig_7_sel().bits(3) // rx -> GLB_UART_SIG_FUN_UART0_RXD
-    });
-    // todo: fifo
-    // dp.UART.uart_fifo_config_1.modify(|_, w| unsafe { w
-    //     .tx_fifo_th().bits(16 - 1)
-    // });
+    
+    let parts = dp.GLB.split();
+    let pin16 = parts.pin16.into_uart_sig0();
+    let pin7 = parts.pin7.into_uart_sig7();
+    let mux0 = parts.uart_mux0.into_uart0_tx();
+    let mux7 = parts.uart_mux7.into_uart0_rx();
+    let mut config = Config::default();
+    config.baudrate = 20000.Bd();
+    let mut serial = Serial::uart0(
+        dp.UART,
+        config,
+        ((pin16, mux0), (pin7, mux7))
+    );
     loop {
-        // write data
-        // while dp.UART.uart_fifo_config_1.read().tx_fifo_cnt().bits() == 0 {}
-        dp.UART.uart_fifo_wdata.write(|w| unsafe {
-            w.bits(b'R' as u32)
-        });
-        dp.UART.uart_fifo_wdata.write(|w| unsafe {
-            w.bits(b'U' as u32)
-        });
-        dp.UART.uart_fifo_wdata.write(|w| unsafe {
-            w.bits(b'S' as u32)
-        });
-        dp.UART.uart_fifo_wdata.write(|w| unsafe {
-            w.bits(b'T' as u32)
-        });
-        while dp.UART.uart_status.read().sts_utx_bus_busy().bit_is_set() {}
+        serial.try_write(b'R').ok();
+        serial.try_flush().ok();
+        serial.try_write(b'U').ok();
+        serial.try_flush().ok();
+        serial.try_write(b'S').ok();
+        serial.try_flush().ok();
+        serial.try_write(b'T').ok();
+        serial.try_flush().ok();
     }
 }
